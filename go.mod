d5e51d5ca05bb04443a6a1e5c8afd7f33b0de0e4
hoomanist <hham.group12@gmail.com>
2021-01-19 12:32:55 +0330

    added compression and logging

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hoomanist/go-web-template
 go 1.15
 
 require (
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/mattn/go-sqlite3 v1.14.6 // indirect
 	gorm.io/driver/sqlite v1.1.4
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"log"
 	"net/http"
+	"os"
 )
 
 type App struct {
@@ -28,7 +30,8 @@ func NewApp() App {
 func main() {
 	app := NewApp()
 	app.SetupRoutes()
-	http.Handle("/", app.router)
+	router := handlers.LoggingHandler(os.Stdout, app.router)
+	http.Handle("/", handlers.CompressHandler(router))
 	fmt.Println("starting the server...")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
